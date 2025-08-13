832a2f359c7fcbb22f11193e8e5ef242f5392190
riaziiii <lopezarianne014@gmail.com>
2025-08-13 14:53:41 +0800

    security save firebase

--- firebase.js
+++ firebase.js
@@ -6,15 +6,16 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-
 // <-- Use your existing Firebase config here -->
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY, 
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId:process.env.FIREBASE_APP_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
 };
-  
+
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
