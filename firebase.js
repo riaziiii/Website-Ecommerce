e4d64e1f71c2f6678d61d45ea95a48a8ecb5209b
riaziiii <lopezarianne014@gmail.com>
2025-08-13 15:31:40 +0800

    security for firebase

--- firebase.js
+++ firebase.js
@@ -5,16 +5,10 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-
 
 // <-- Use your existing Firebase config here -->
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
-};
 
+
+const response = await fetch("/api/firebase-config");
+const firebaseConfig = await response.json();
 
 
 const app = initializeApp(firebaseConfig);
