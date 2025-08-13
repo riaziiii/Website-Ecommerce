5fe9d051d2c7565d38eb763cecb21166f78179ea
riaziiii <lopezarianne014@gmail.com>
2025-08-13 14:27:25 +0800

    save the firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-
 
 // <-- Use your existing Firebase config here -->
 const firebaseConfig = {
-  apiKey: "AIzaSyAYLQqfR2nIGmGRjbMaDFR4ZldHMBx_pq4",
-  authDomain: "website-ecommerce-4d60f.firebaseapp.com",
-  projectId: "website-ecommerce-4d60f",
-  storageBucket: "website-ecommerce-4d60f.firebasestorage.app",
-  messagingSenderId: "595639631144",
-  appId: "1:595639631144:web:9c338c631666320c26a41d",
-  databaseURL: "https://website-ecommerce-4d60f-default-rtdb.firebaseio.com/"
+  apiKey: process.env.FIREBASE_API_KEY || "AIzaSyAYLQqfR2nIGmGRjbMaDFR4ZldHMBx_pq4", 
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "website-ecommerce-4d60f.firebaseapp.com",
+  projectId: process.env.PROJECT_ID || "website-ecommerce-4d60f",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "website-ecommerce-4d60f.firebasestorage.app",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "595639631144",
+  appId:process.env.FIREBASE_APP_ID ||"1:595639631144:web:9c338c631666320c26a41d",
+  databaseURL: process.env.FIREBASE_DATABASE_URL || "https://website-ecommerce-4d60f-default-rtdb.firebaseio.com/"
 };
 
 const app = initializeApp(firebaseConfig);
