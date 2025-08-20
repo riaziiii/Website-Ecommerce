37596092c7f3a86e8194efdd95232a497c832e6d
riaziiii <lopezarianne014@gmail.com>
2025-08-20 15:32:36 +0800

    testin

--- firebase.js
+++ firebase.js
@@ -3,12 +3,21 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAYLQqfR2nIGmGRjbMaDFR4ZldHMBx_pq4",
+  authDomain: "website-ecommerce-4d60f.firebaseapp.com",
+  projectId: "website-ecommerce-4d60f",
+  storageBucket: "website-ecommerce-4d60f.appspot.com",
+  messagingSenderId: "595639631144",
+  appId: "1:595639631144:web:9c338c631666320c26a41d",
+  databaseURL: "https://website-ecommerce-4d60f-default-rtdb.firebaseio.com/"
+};
 
-
-
+/*
 const response = await fetch("/api/firebase-config");
 const firebaseConfig = await response.json();
-
+*/
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
